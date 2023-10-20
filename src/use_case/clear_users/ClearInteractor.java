ea3e0c1ee9aeab0c849678748644216a72adf574
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-10-20 11:40:40 -0400

    Add InputBoundary interface and ClearInteractor

--- src/use_case/clear_users/ClearInteractor.java
+++ src/use_case/clear_users/ClearInteractor.java
@@ -2,5 +2,23 @@ package use_case.clear_users;
 
 // TODO Complete me
 
-public class ClearInteractor {
+public class ClearInteractor implements ClearInputBoundary {
+    final ClearUserDataAccessInterface clearUserDataAccessObject;
+    final ClearOutputBoundary clearPresenter;
+
+    public ClearInteractor(ClearUserDataAccessInterface clearUserDataAccessInterface, ClearOutputBoundary clearPresenter) {
+        this.clearUserDataAccessObject = clearUserDataAccessInterface;
+        this.clearPresenter = clearPresenter;
+    }
+
+    @Override
+    public void execute() {
+        if (clearUserDataAccessObject.noUsers()) {
+            clearPresenter.prepareFailView("No users to delete.");
+        } else {
+            ClearOutputData clearOutputData = new ClearOutputData(clearUserDataAccessObject.getAll());
+            clearUserDataAccessObject.clearUsers();
+            clearPresenter.prepareSuccessView(clearOutputData);
+        }
+    }
 }
