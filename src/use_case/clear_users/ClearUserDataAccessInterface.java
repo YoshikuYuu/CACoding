91222c8497c5d1cc272f6418157f14a65244b996
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-10-20 11:42:36 -0400

    Add DA Interface and Clear DAO

--- src/use_case/clear_users/ClearUserDataAccessInterface.java
+++ src/use_case/clear_users/ClearUserDataAccessInterface.java
@@ -2,5 +2,10 @@ package use_case.clear_users;
 
 // TODO Complete me
 
+import entity.User;
+
 public interface ClearUserDataAccessInterface {
+    boolean noUsers();
+    String[] getAll();
+    void clearUsers();
 }
